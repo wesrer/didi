efd4ec9ccb86c0cd889f725582e06c706af28555
wesrer <sayakchatterjee1309@gmail.com>
2020-05-26 17:13:25 +0530

    added url function defintions

--- didi.c
+++ didi.c
@@ -3,6 +3,18 @@
 # include <stdbool.h>
 # include <argp.h>
 
+bool is_valid_url(char *url) {
+    return false;
+}
+
+bool is_url_in_db(char *url) {
+    return false;
+}
+
+int add_url(char *url){
+    return EXIT_FAILURE;
+}
+
 // Notice that there is no visible call to this function.
 // This is because `parse_opt` is a __callback function__.
 // Instead `Argp` is calling this function on our behalf when it processes
@@ -18,21 +30,39 @@
 //   state: This keeps Argp's state as we iterate the callback function
 static int parse_opt (int key, char *arg, struct argp_state *state) {
     switch (key) {
-        case 'a': printf("selected add");
-                  break;
+        case 'a': {
+            if (!is_valid_url(arg)){
+                argp_failure(state, 1, 0, "invalid URL");
+            }
+            if (is_url_in_db(arg)) {
+                argp_failure(state, 1, 0, "URL already added");
+            }
+            // TODO: If it already exists, don't add it again
+            printf("selected %s", arg);
+            break;
+        }
     }
 
     return EXIT_SUCCESS; // EXIT_FAILURE will stop Argp and return FAILURE
 }
 
+
 int main(int argc, char **argv) {
 
     struct argp_option options[] = {
         // Field options:
+        //   1st -> long
         //   2nd -> short
+        //   3rd -> name of the argument to this option
+        //   4th -> A set of flags that change how our option works
         //   5th -> "Description of the option to be shown on help"
-        {0, 'a', 0, 0, "Show add on the screen"},
-        {0}
+        { "add", 'a', "URL", 0, "Add an URL that you have downloaded from" },
+        { "check",
+          'c',
+          "URL",
+          0 ,
+          "check if you have aleardy downloaded from this URL" },
+        { 0 }
     };
 
     // This is the most important struct here
@@ -49,3 +79,4 @@ int main(int argc, char **argv) {
     return argp_parse(&argp, argc, argv, 0, 0, 0);
     /* return EXIT_SUCCESS; */
 }
+
